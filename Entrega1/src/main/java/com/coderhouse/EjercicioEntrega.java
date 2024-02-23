56ab4785f299e4e4a658c049985241e8208a90d3
punigonzalez <punigonzalez07@gmail.com>
2024-02-23 11:28:13 -0300

    Ejericio entrega comienzo

--- Entrega1/src/main/java/com/coderhouse/EjercicioEntrega.java
+++ Entrega1/src/main/java/com/coderhouse/EjercicioEntrega.java
@@ -3,7 +3,15 @@ package com.coderhouse;
 public class EjercicioEntrega {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		float numero = (float) 9.145;
+		
+		int numeroEntero = (int) numero;
+		byte numeroDecimal = (byte) ((numero - numeroEntero) * 100);
+		
+		System.out.println("Numero: " + numero );
+		System.out.println("Parte entera: " + numeroEntero );
+		System.out.println("Parte decimal: " + numeroDecimal );
+		
 
 	}
 
